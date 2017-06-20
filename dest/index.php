5f3b26387d5cc6fee39eaabfeb894e502df848c2
Brian Roos <brianroos94@gmail.com>
2017-06-20 15:44:21 +0200

    Pages layout: login and applications overview

---
+++ dest/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Werkgelegenheid On Demand</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge; chrome=1">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <link rel="stylesheet" type="text/css" href="css/style.min.css">
+</head>
+<body>
+  <div class="page index">
+
+    <div class="intro">
+      <div class="row">
+        <div class="columns">
+          <img src="img/logo-big.png" alt="Werkgelegenheid On Demand">
+
+          <div class="form login-form">
+            <form action="#" method="post">
+              <div class="form-row">
+                <label for="email">E-mail</label>
+                <input type="email" name="email" required>
+              </div>
+              <div class="form-row">
+                <label for="password">Wachtwoord</label>
+                <input type="password" name="password" required>
+              </div>
+              <div class="form-row submit-row">
+                <input type="submit" name="submit" value="Inloggen">
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <script src="js/vendor.min.js"></script>
+  <script src="js/script.min.js"></script>
+</body>
+</html>
---
+++ source/applications-overview.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Werkgelegenheid On Demand</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge; chrome=1">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <link rel="stylesheet" type="text/css" href="css/style.min.css">
+</head>
+<body>
+  <div class="page applications-overview">
+
+    <div class="top-navigation">
+      <div class="row">
+        <div class="columns logo small-12">
+          <a href="search.php"><img src="img/logo.png" alt="Werkgelegenheid On Demand"></a>
+        </div>
+        <div class="columns nav-buttons small-12">
+          <a class="open-menu" href="#"><img src="img/menu.png" alt=""></a>
+        </div>
+      </div>
+    </div>
+    <div class="main-navigation">
+      <a class="close-menu" href="#"><img src="img/close.png" alt=""></a>
+      <nav>
+        <ul>
+          <li><a href="#">Nieuwe zoekopdracht</a></li>
+          <li><a href="#">Overzicht aanmeldingen</a></li>
+          <li><a href="#">Uitloggen</a></li>
+        </ul>
+      </nav>
+    </div>
+
+    <div class="content applications-information">
+      <div class="row">
+        <div class="columns">
+          <div class="sections">
+            <div class="section">
+              <div class="a-options">
+                <div class="options-wrap">
+                  <div class="options-inside">
+                    <a class="delete-application" href="#"><img src="img/delete.png" alt=""></a>
+                    <a class="notify-application" href="#"><img src="img/notify.png" alt=""></a>
+                  </div>
+                </div>
+              </div>
+              <div class="a-information">
+                <h4>Bedrijf A</h4>
+                <ul>
+                  <li>Winkel, voorraad bijvullen</li>
+                  <li>Woensdag 7 juni, 2 <span>uur werk</span>, 10 <span>p.u.</span></li>
+                  <li>Binnenwegplein 50-52, 3012 KA Rotterdam</li>
+                </ul>
+              </div>
+            </div>
+            <div class="section">
+              <div class="a-options">
+                <div class="options-wrap">
+                  <div class="options-inside">
+                    <a class="delete-application" href="#"><img src="img/delete.png" alt=""></a>
+                  </div>
+                </div>
+              </div>
+              <div class="a-information">
+                <h4>Bedrijf B</h4>
+                <ul>
+                  <li>Winkel, voorraad bijvullen</li>
+                  <li>Woensdag 7 juni, 2 <span>uur werk</span>, 10 <span>p.u.</span></li>
+                  <li>Binnenwegplein 50-52, 3012 KA Rotterdam</li>
+                </ul>
+              </div>
+            </div>
+            <div class="section">
+              <div class="a-options">
+                <div class="options-wrap">
+                  <div class="options-inside">
+                    <a class="delete-application" href="#"><img src="img/delete.png" alt=""></a>
+                  </div>
+                </div>
+              </div>
+              <div class="a-information">
+                <h4>Bedrijf C</h4>
+                <ul>
+                  <li>Winkel, voorraad bijvullen</li>
+                  <li>Woensdag 7 juni, 2 <span>uur werk</span>, 10 <span>p.u.</span></li>
+                  <li>Binnenwegplein 50-52, 3012 KA Rotterdam</li>
+                </ul>
+              </div>
+            </div>
+            <div class="section">
+              <div class="a-options">
+                <div class="options-wrap">
+                  <div class="options-inside">
+                    <a class="delete-application" href="#"><img src="img/delete.png" alt=""></a>
+                  </div>
+                </div>
+              </div>
+              <div class="a-information">
+                <h4>Bedrijf D</h4>
+                <ul>
+                  <li>Winkel, voorraad bijvullen</li>
+                  <li>Woensdag 7 juni, 2 <span>uur werk</span>, 10 <span>p.u.</span></li>
+                  <li>Binnenwegplein 50-52, 3012 KA Rotterdam</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <script src="js/vendor.min.js"></script>
+  <script src="js/script.min.js"></script>
+</body>
+</html>
